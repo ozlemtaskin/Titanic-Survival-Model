748eb6ae16388a6bb7769bb9d2f32711b0baa2e3
Özlem Taşkın <95315318+ozlemtaskin@users.noreply.github.com>
2022-02-24 01:20:53 +0300

    Add files via upload

---
+++ Titanic.py
@@ -0,0 +1,317 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+#!pip install missingno
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix, classification_report, plot_roc_curve
+from sklearn.model_selection import train_test_split, cross_validate
+
+def load():
+    data = pd.read_csv(r"C:\Users\hp\PycharmProjects\pythonProject\2. HAFTA\titanic.csv")
+    return data
+
+df_ = load()
+
+df= df_.copy()
+#Genel resmi inceleme
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+#Numerik ve kategorik değişkenleri yakalama
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+#Numerik ve kategorik değişken analizleri
+
+def cat_summary(dataframe, col_name, plot=False):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+
+    if plot:
+        sns.countplot(x=dataframe[col_name], data=dataframe)
+        plt.show()
+
+for col in cat_cols:
+    cat_summary(df, col, plot=True)
+
+
+def num_summary(dataframe, numerical_col, plot=False):
+    quantiles = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99]
+    print(dataframe[numerical_col].describe(quantiles).T)
+
+    if plot:
+        dataframe[numerical_col].hist(bins=50)
+        plt.xlabel(numerical_col)
+        plt.title(numerical_col)
+        plt.show()
+
+    print("#####################################")
+
+
+for col in num_cols:
+    num_summary(df, col, plot=True)
+
+
+#Aykırı gözlem analizi
+
+def outlier_thresholds(dataframe, col_name, q1=0.05, q3=0.95):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+for col_names in num_cols:
+    print(col_names, check_outlier(df, col_names))
+
+def grab_outliers(dataframe, col_name, index=False):
+    low, up = outlier_thresholds(dataframe, col_name)
+
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+
+for col_names in num_cols:
+    print(col_names ,"\n", grab_outliers(df, col_names))
+
+
+def remove_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+
+df=remove_outlier(df,"Fare")
+
+df.isnull().sum()
+
+df["Embarked"]=df["Embarked"].fillna(df["Embarked"].mode()[0])
+
+df["Age"]= df["Age"].fillna(df.groupby("Sex")["Age"].transform("mean"))
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+na_columns = missing_values_table(df, na_name=True)
+
+
+corr = df[num_cols].corr()
+corr
+
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.70):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={'figure.figsize': (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+high_correlated_cols(df, plot=True)
+
+
+df["NEW_AGE_CAT"]=pd.cut(x=df["Age"], bins= [0,18,30,50,100], labels=["child","young","mature","senior"])
+
+#Agexsex
+df.head()
+df.loc[(df["NEW_AGE_CAT"]=="child") & (df["Sex"]=="female"),"NEW_SEX_CAT"]= "child female"
+df.loc[(df["NEW_AGE_CAT"]=="child") & (df["Sex"]=="male"),"NEW_SEX_CAT"]= "child male"
+df.loc[(df["NEW_AGE_CAT"]=="young") & (df["Sex"]=="female"),"NEW_SEX_CAT"]= "young female"
+df.loc[(df["NEW_AGE_CAT"]=="young") & (df["Sex"]=="male"),"NEW_SEX_CAT"]= "young male"
+df.loc[(df["NEW_AGE_CAT"]=="mature") & (df["Sex"]=="female"),"NEW_SEX_CAT"]= "mature female"
+df.loc[(df["NEW_AGE_CAT"]=="mature") & (df["Sex"]=="male"),"NEW_SEX_CAT"]= "mature male"
+df.loc[(df["NEW_AGE_CAT"]=="senior") & (df["Sex"]=="female"),"NEW_SEX_CAT"]= "senior female"
+df.loc[(df["NEW_AGE_CAT"]=="senior") & (df["Sex"]=="male"),"NEW_SEX_CAT"]= "senior male"
+
+#Family size
+df["NEW_FAMSİZE_CAT"]= df["SibSp"]+ df["Parch"]
+
+#Alone
+df.loc[(df["NEW_FAMSİZE_CAT"]==0) ,"NEW_ALONE_CAT"]= 1
+df.loc[(df["NEW_FAMSİZE_CAT"]>0)  ,"NEW_ALONE_CAT"]= 0
+
+df.head()
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+#Encoding İşlemleri
+
+#Label Encoding
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+binary_cols = [col for col in df.columns if df[col].dtypes == "O" and df[col].nunique() == 2]
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+
+#One Hot Encoder
+
+cat_cols = [col for col in cat_cols if col not in binary_cols and col not in ["Survived"]]
+cat_cols
+
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+df = one_hot_encoder(df, cat_cols, drop_first=True)
+
+#Standartlaştırma
+
+#Standard Scaler
+
+
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+df[num_cols].head()
+
+df.head()
+
+#Model
+
+y = df["Survived"]
+X = df.drop(["Survived","PassengerId","Name","Ticket","Cabin"], axis=1)
+
+log_model = LogisticRegression().fit(X, y)
+
+
+cv_results = cross_validate(log_model,
+                            X, y,
+                            cv=5,
+                            scoring=["accuracy", "precision", "recall", "f1", "roc_auc"])
+
+
+
+cv_results['test_accuracy'].mean()
+
+cv_results['test_precision'].mean()
+
+cv_results['test_recall'].mean()
+
+cv_results['test_f1'].mean()
+
+cv_results['test_roc_auc'].mean()
+
+
+
+
+
+
+
+
+
